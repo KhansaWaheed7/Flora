1456c56ddb78fcc8e57c38bd340cc15dcd660545
Khansa Waheed <khanawaheed703@gmail.com>
2026-07-16 15:54:36 +0500

    Implement menstrual cycle CRUD module

---
+++ server/src/models/Cycle.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+
+const cycleSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+
+    periodStart: {
+      type: Date,
+      required: true,
+    },
+
+    periodEnd: {
+      type: Date,
+      required: true,
+    },
+
+    cycleLength: {
+      type: Number,
+      required: true,
+      min: 15,
+      max: 60,
+    },
+
+    periodLength: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 10,
+    },
+
+    symptoms: [
+      {
+        type: String,
+        enum: [
+          "cramps",
+          "headache",
+          "bloating",
+          "fatigue",
+          "back_pain",
+          "breast_tenderness",
+          "mood_swings",
+          "acne",
+          "nausea",
+          "insomnia",
+          "none",
+        ],
+      },
+    ],
+
+    notes: {
+      type: String,
+      trim: true,
+      maxlength: 500,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Cycle", cycleSchema);
---
+++ server/src/routes/cycle.routes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { protect } = require("../middlewares/auth.middleware");
+
+const {
+  createCycle,
+  getCycles,
+  getCycle,
+  updateCycle,
+  deleteCycle,
+} = require("../controllers/cycle.controller");
+
+router.use(protect);
+
+router.post("/", createCycle);
+
+router.get("/", getCycles);
+
+router.get("/:id", getCycle);
+
+router.put("/:id", updateCycle);
+
+router.delete("/:id", deleteCycle);
+
+module.exports = router;
