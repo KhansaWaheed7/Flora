f833eac1a909f1ed6da88eecf49fe0faf4b187a0
Khansa Waheed <khanawaheed703@gmail.com>
2026-07-28 19:50:23 +0500

    Add Google OAuth authentication

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import "./index.css";
 import App from "./App.jsx";
@@ -11,6 +12,9 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+    <GoogleOAuthProvider
+    clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
+  >
       <AuthProvider>
         <App />
          <Toaster
@@ -42,6 +46,7 @@ createRoot(document.getElementById("root")).render(
       }}
     />
       </AuthProvider>
+      </GoogleOAuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -98,3 +98,17 @@ export const resendVerification = async (email) => {
 
   return response.data;
 };
+
+/*
+Google Login
+*/
+
+export const googleLogin = async (token) => {
+
+  const response = await api.post("/auth/google", {
+    token,
+  });
+
+  return response.data;
+
+};
