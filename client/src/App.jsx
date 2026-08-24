63001220438269f32a3fc8856871846e92058433
MOMNANWAZ <momnanwaz@github.com>
2026-08-24 20:41:49 +0500

    Update project and PCOS integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
+
 import LandingPage from "./pages/public/Landing/LandingPage";
-import TermsAndConditions from "./pages/public/TermsAndConditions"
-import PrivacyPolicy from "./pages/public/PrivacyPolicy"
+import TermsAndConditions from "./pages/public/TermsAndConditions";
+import PrivacyPolicy from "./pages/public/PrivacyPolicy";
+
 import LoginPage from "./pages/auth/Login/LoginPage";
 import RegisterPage from "./pages/auth/Register/RegisterPage";
 import ForgotPasswordPage from "./pages/auth/ForgetPassword/ForgotPasswordPage";
 import ResetPasswordPage from "./pages/auth/ResetPassword/ResetPasswordPage";
 import VerifyEmailPage from "./pages/auth/VerifyEmail/VerifyEmailPage";
+
 import DashboardPage from "./pages/dashboard/DashboardPage";
 import ProfilePage from "./pages/profile/ProfilePage";
 import ProfileDetailsPage from "./pages/profile/ProfileDetailsPage";
 import EditProfilePage from "./pages/profile/EditProfilePage";
 import SettingsPage from "./pages/settings/SettingsPage";
+
 import CycleTrackerDashboard from "./pages/cycle-tracker/CycleTrackerDashboard";
 import LogPeriod from "./pages/cycle-tracker/LogPeriod";
 import EditCycle from "./pages/cycle-tracker/EditCycle";
@@ -20,6 +24,7 @@ import CycleHistory from "./pages/cycle-tracker/CycleHistory";
 import CycleDetails from "./pages/cycle-tracker/CycleDetails";
 import Predictions from "./pages/cycle-tracker/Predictions";
 import CycleStatistics from "./pages/cycle-tracker/CycleStatistics";
+
 import PCOSIntro from "./pages/pcos-detection/PCOSIntro";
 import PCOSQuestionnaire from "./pages/pcos-detection/PCOSQuestionnaire";
 import PCOSAnalyzing from "./pages/pcos-detection/PCOSAnalyzing";
@@ -27,6 +32,7 @@ import PCOSResult from "./pages/pcos-detection/PCOSResult";
 import PCOSRecommendations from "./pages/pcos-detection/PCOSRecommendations";
 import PCOSHistory from "./pages/pcos-detection/PCOSHistory";
 import PCOSDetails from "./pages/pcos-detection/PCOSDetails";
+
 import PregnancyDashboard from "./pages/pregnancy/PregnancyDashboard";
 import RegisterPregnancy from "./pages/pregnancy/RegisterPregnancy";
 import PregnancyOverview from "./pages/pregnancy/PregnancyOverview";
@@ -35,233 +41,397 @@ import WeeklyGuide from "./pages/pregnancy/WeeklyGuide";
 import WeekDetails from "./pages/pregnancy/WeekDetails";
 import PregnancyReminders from "./pages/pregnancy/PregnancyReminders";
 import ReminderDetails from "./pages/pregnancy/ReminderDetails";
-import ProtectedRoute from "./routes/ProtectedRoute";
 
+import FindDoctor from "./pages/chat/FindDoctor";
+import DoctorProfile from "./pages/chat/DoctorProfile";
+import ConsultationRequestConfirmation from "./pages/chat/ConsultationRequestConfirmation";
+import MyConsultations from "./pages/chat/MyConsultations";
+import PendingConsultation from "./pages/chat/PendingConsultation";
+import RejectedConsultation from "./pages/chat/RejectedConsultation";
+import ChatWithDoctor from "./pages/chat/ChatWithDoctor";
+import ConsultationClosed from "./pages/chat/ConsultationClosed";
+
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/terms" element={<TermsAndConditions />} />
-        <Route path="/privacy" element={<PrivacyPolicy />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
-        <Route path="/verify-email/:token" element={<VerifyEmailPage />} />
-        <Route
-  path="/dashboard"
-  element={
-    <ProtectedRoute>
-      <DashboardPage />
-    </ProtectedRoute>
-  }
-/>
-        <Route path="/profile/details" element={<ProtectedRoute>
-      <ProfileDetailsPage />
-    </ProtectedRoute>} />
-        <Route
-  path="/profile"
-  element={
-    <ProtectedRoute>
-      <ProfilePage />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/profile/edit"
-  element={
-    <ProtectedRoute>
-      <EditProfilePage />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/settings"
-  element={
-    <ProtectedRoute>
-      <SettingsPage />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/cycle-tracker"
-  element={
-    <ProtectedRoute>
-      <CycleTrackerDashboard />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/cycle-tracker/log"
-  element={
-    <ProtectedRoute>
-      <LogPeriod />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/cycle-tracker/history"
-  element={
-    <ProtectedRoute>
-      <CycleHistory />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/cycle-tracker/predictions"
-  element={
-    <ProtectedRoute>
-      <Predictions />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/cycle-tracker/statistics"
-  element={
-    <ProtectedRoute>
-      <CycleStatistics />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/cycle-tracker/:id/edit"
-  element={
-    <ProtectedRoute>
-      <EditCycle />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/cycle-tracker/:id"
-  element={
-    <ProtectedRoute>
-      <CycleDetails />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/pcos-detection"
-  element={
-    <ProtectedRoute>
-      <PCOSIntro />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/pcos-detection/assessment"
-  element={
-    <ProtectedRoute>
-      <PCOSQuestionnaire />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/pcos-detection/analyzing"
-  element={
-    <ProtectedRoute>
-      <PCOSAnalyzing />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/pcos-detection/result"
-  element={
-    <ProtectedRoute>
-      <PCOSResult />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/pcos-detection/recommendations"
-  element={
-    <ProtectedRoute>
-      <PCOSRecommendations />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/pcos-detection/history"
-  element={
-    <ProtectedRoute>
-      <PCOSHistory />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/pcos-detection/:id"
-  element={
-    <ProtectedRoute>
-      <PCOSDetails />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/pregnancy"
-  element={
-    <ProtectedRoute>
-      <PregnancyDashboard />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/pregnancy/update"
-  element={
-    <ProtectedRoute>
-      <RegisterPregnancy />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/pregnancy/overview"
-  element={
-    <ProtectedRoute>
-      <PregnancyOverview />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/pregnancy/timeline"
-  element={
-    <ProtectedRoute>
-      <PregnancyTimeline />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/pregnancy/weekly-guide"
-  element={
-    <ProtectedRoute>
-      <WeeklyGuide />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/pregnancy/weekly-guide/:week"
-  element={
-    <ProtectedRoute>
-      <WeekDetails />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/pregnancy/reminders"
-  element={
-    <ProtectedRoute>
-      <PregnancyReminders />
-    </ProtectedRoute>
-  }
-/>
-        <Route
-  path="/pregnancy/reminders/:id"
-  element={
-    <ProtectedRoute>
-      <ReminderDetails />
-    </ProtectedRoute>
-  }
-/>
-      </Routes>
+    <Routes>
+      {/* ==================== PUBLIC ROUTES ==================== */}
 
+      <Route path="/" element={<LandingPage />} />
+
+      <Route path="/terms" element={<TermsAndConditions />} />
+
+      <Route path="/privacy" element={<PrivacyPolicy />} />
+
+      <Route path="/login" element={<LoginPage />} />
+
+      <Route path="/register" element={<RegisterPage />} />
+
+      <Route
+        path="/forgot-password"
+        element={<ForgotPasswordPage />}
+      />
+
+      <Route
+        path="/reset-password/:token"
+        element={<ResetPasswordPage />}
+      />
+
+      <Route
+        path="/verify-email/:token"
+        element={<VerifyEmailPage />}
+      />
+
+      {/* ==================== DASHBOARD ==================== */}
+
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <DashboardPage />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* ==================== PROFILE ==================== */}
+
+      <Route
+        path="/profile/details"
+        element={
+          <ProtectedRoute>
+            <ProfileDetailsPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <ProfilePage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/profile/edit"
+        element={
+          <ProtectedRoute>
+            <EditProfilePage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/settings"
+        element={
+          <ProtectedRoute>
+            <SettingsPage />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* ==================== CYCLE TRACKER ==================== */}
+
+      <Route
+        path="/cycle-tracker"
+        element={
+          <ProtectedRoute>
+            <CycleTrackerDashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/cycle-tracker/log"
+        element={
+          <ProtectedRoute>
+            <LogPeriod />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/cycle-tracker/history"
+        element={
+          <ProtectedRoute>
+            <CycleHistory />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/cycle-tracker/predictions"
+        element={
+          <ProtectedRoute>
+            <Predictions />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/cycle-tracker/statistics"
+        element={
+          <ProtectedRoute>
+            <CycleStatistics />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/cycle-tracker/:id/edit"
+        element={
+          <ProtectedRoute>
+            <EditCycle />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/cycle-tracker/:id"
+        element={
+          <ProtectedRoute>
+            <CycleDetails />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* ==================== PCOS DETECTION ==================== */}
+
+      <Route
+        path="/pcos-detection"
+        element={
+          <ProtectedRoute>
+            <PCOSIntro />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/pcos-detection/assessment"
+        element={
+          <ProtectedRoute>
+            <PCOSQuestionnaire />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/pcos-detection/analyzing"
+        element={
+          <ProtectedRoute>
+            <PCOSAnalyzing />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/pcos-detection/result"
+        element={
+          <ProtectedRoute>
+            <PCOSResult />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/pcos-detection/recommendations"
+        element={
+          <ProtectedRoute>
+            <PCOSRecommendations />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/pcos-detection/history"
+        element={
+          <ProtectedRoute>
+            <PCOSHistory />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/pcos-detection/:id"
+        element={
+          <ProtectedRoute>
+            <PCOSDetails />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* ==================== PREGNANCY ==================== */}
+
+      <Route
+        path="/pregnancy"
+        element={
+          <ProtectedRoute>
+            <PregnancyDashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/pregnancy/update"
+        element={
+          <ProtectedRoute>
+            <RegisterPregnancy />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/pregnancy/overview"
+        element={
+          <ProtectedRoute>
+            <PregnancyOverview />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/pregnancy/timeline"
+        element={
+          <ProtectedRoute>
+            <PregnancyTimeline />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/pregnancy/weekly-guide"
+        element={
+          <ProtectedRoute>
+            <WeeklyGuide />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/pregnancy/weekly-guide/:week"
+        element={
+          <ProtectedRoute>
+            <WeekDetails />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/pregnancy/reminders"
+        element={
+          <ProtectedRoute>
+            <PregnancyReminders />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/pregnancy/reminders/:id"
+        element={
+          <ProtectedRoute>
+            <ReminderDetails />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* ==================== CHAT ==================== */}
+
+      {/* MAIN CHAT PAGE */}
+      <Route
+        path="/chat"
+        element={
+          <ProtectedRoute>
+            <MyConsultations />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* FIND DOCTORS */}
+      <Route
+        path="/chat/doctors"
+        element={
+          <ProtectedRoute>
+            <FindDoctor />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* DOCTOR PROFILE */}
+      <Route
+        path="/chat/doctors/:id"
+        element={
+          <ProtectedRoute>
+            <DoctorProfile />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* CONSULTATION REQUEST SENT */}
+      <Route
+        path="/chat/request-sent"
+        element={
+          <ProtectedRoute>
+            <ConsultationRequestConfirmation />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* MY CONSULTATIONS */}
+      <Route
+        path="/chat/my-consultations"
+        element={
+          <ProtectedRoute>
+            <MyConsultations />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* PENDING CONSULTATION */}
+      <Route
+        path="/chat/:id/pending"
+        element={
+          <ProtectedRoute>
+            <PendingConsultation />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* REJECTED CONSULTATION */}
+      <Route
+        path="/chat/:id/rejected"
+        element={
+          <ProtectedRoute>
+            <RejectedConsultation />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* CLOSED CONSULTATION */}
+      <Route
+        path="/chat/:id/closed"
+        element={
+          <ProtectedRoute>
+            <ConsultationClosed />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* ACTIVE CHAT WITH DOCTOR */}
+      <Route
+        path="/chat/:id"
+        element={
+          <ProtectedRoute>
+            <ChatWithDoctor />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* ==================== FALLBACK ==================== */}
+
+      <Route path="*" element={<Navigate to="/dashboard" replace />} />
+    </Routes>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,45 +6,49 @@ import { Toaster } from "react-hot-toast";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { SocketProvider } from "./context/SocketContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <GoogleOAuthProvider
-    clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
-  >
-      <AuthProvider>
-        <App />
-         <Toaster
-      position="top-right"
-      toastOptions={{
-        duration: 3500,
+      <GoogleOAuthProvider
+        clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
+      >
+        <AuthProvider>
+          <SocketProvider>
+            <App />
 
-        style: {
-          borderRadius: "14px",
-          background: "#fff",
-          color: "#3D3939",
-          border: "1px solid #F0DCE4",
-          padding: "14px 16px",
-        },
+            <Toaster
+              position="top-right"
+              toastOptions={{
+                duration: 3500,
 
-        success: {
-          iconTheme: {
-            primary: "#F33B7D",
-            secondary: "#fff",
-          },
-        },
+                style: {
+                  borderRadius: "14px",
+                  background: "#fff",
+                  color: "#3D3939",
+                  border: "1px solid #F0DCE4",
+                  padding: "14px 16px",
+                },
 
-        error: {
-          iconTheme: {
-            primary: "#EF4444",
-            secondary: "#fff",
-          },
-        },
-      }}
-    />
-      </AuthProvider>
+                success: {
+                  iconTheme: {
+                    primary: "#F33B7D",
+                    secondary: "#fff",
+                  },
+                },
+
+                error: {
+                  iconTheme: {
+                    primary: "#EF4444",
+                    secondary: "#fff",
+                  },
+                },
+              }}
+            />
+          </SocketProvider>
+        </AuthProvider>
       </GoogleOAuthProvider>
     </BrowserRouter>
   </StrictMode>
